4bcc27f1580e0ee0f29ef784721621dd2961c8d7
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-10-28 15:46:02 -0600

    added mp3s

--- src/app/mp3-player/mp3-player.component.ts
+++ src/app/mp3-player/mp3-player.component.ts
@@ -11,7 +11,7 @@ export class Mp3PlayerComponent implements OnInit {
 
   msaapDisplayTitle = true;
   msaapDisplayPlayList = true;
-  msaapPageSizeOptions = [8];
+  msaapPageSizeOptions = [9];
   msaapDisplayVolumeControls = true;
   msaapDisplayRepeatControls = true;
   msaapDisplayArtist = true;
@@ -21,52 +21,58 @@ export class Mp3PlayerComponent implements OnInit {
     // Material Style Advance Audio Player Playlist
 
     {
-      title: 'Elena Beats',
-      link: 'http://jazzyspoon.com/tools/BEATS%20FOR%20ELENA%201.mp3',
-      artist: 'Jazzyspoon',
-      duration: '3:00',
+      title: 'Jam On It (inst)',
+      link: 'http://jazzyspoon.com/tools/HalloJams/09%20-%20Jam%20On%20It%20(12-Inch%20Instrumental).mp3',
+      artist: 'Newcleus',
+      duration: 3,
     },
     {
-      title: 'Washing machine beats',
-      link: 'http://jazzyspoon.com/tools/wm_drone%20jazzyspoon.mp3',
-      artist: 'Artist',
-      duration: '3:00',
-    },
-    {
-      title: 'Elena Beats',
-      link: 'http://jazzyspoon.com/tools/BEATS%20FOR%20ELENA%201.mp3',
-      artist: 'Jazzyspoon',
-      duration: '3:00',
-    },
-    {
-      title: 'Washing machine beats',
-      link: 'http://jazzyspoon.com/tools/wm_drone%20jazzyspoon.mp3',
-      artist: 'Artist',
-      duration: '3:00',
+      title: 'Insular192',
+      link: 'http://jazzyspoon.com/tools/HalloJams/insular192.mp3',
+      artist: 'Blortblort',
+      duration: 3,
     },
     {
       title: 'Elena Beats',
       link: 'http://jazzyspoon.com/tools/BEATS%20FOR%20ELENA%201.mp3',
       artist: 'Jazzyspoon',
-      duration: '3:00',
+      duration: 3,
     },
     {
-      title: 'Washing machine beats',
-      link: 'http://jazzyspoon.com/tools/wm_drone%20jazzyspoon.mp3',
-      artist: 'Artist',
-      duration: '3:00',
-    },
-    {
-      title: 'Elena Beats',
-      link: 'http://jazzyspoon.com/tools/BEATS%20FOR%20ELENA%201.mp3',
+      title: 'RevLol',
+      link: 'http://jazzyspoon.com/tools/HalloJams/Jazzyspoon%20-%20The%20Alternative%20Mixes%20Of%20My%20Environment%20-%2003%20Rev-LOL-ver%20(Alt%20Mix).mp3',
       artist: 'Jazzyspoon',
-      duration: '3:00',
+      duration: 3,
     },
     {
-      title: 'Washing machine beats',
-      link: 'http://jazzyspoon.com/tools/wm_drone%20jazzyspoon.mp3',
-      artist: 'Artist',
-      duration: '3:00',
+      title: 'Kiddies',
+      link: 'http://jazzyspoon.com/tools/HalloJams/KiDdies.mp3',
+      artist: 'Self',
+      duration: 3,
+    },
+    {
+      title: 'Gone',
+      link: 'http://jazzyspoon.com/tools/HalloJams/Jazzyspoon%20&%20IG88%20-%20Jazzyspoon%20&%20IG88%20-%2001%20Gone.mp3',
+      artist: 'Jazzyspoon and IG88',
+      duration: 3,
+    },
+    {
+      title: 'Sleeper',
+      link: 'http://jazzyspoon.com/tools/HalloJams/08%20-%20Sleeper.mp3',
+      artist: 'Yeah Ghost',
+      duration: 3,
+    },
+    {
+      title: 'Absence',
+      link: 'http://jazzyspoon.com/tools/HalloJams/Jazzyspoon%20-%20Hard%20Drive%20Recovery%20-%2006%20Absence%20(20%20mix).mp3',
+      artist: 'Jazzyspoon',
+      duration: 3,
+    },
+    {
+      title: 'Our Whispered Rants',
+      link: 'http://jazzyspoon.com/tools/HalloJams/Jazzyspoon%20-%20Noise%20and%20Noir%20-%2002%20Our%20Whispered%20Rants.mp3',
+      artist: 'Jazzyspoon',
+      duration: 3,
     },
   ];
 
