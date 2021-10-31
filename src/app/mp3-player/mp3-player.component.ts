7b2952829a0ac28f237f346c0876874c2095f933
Jason Pottorff <77120503+Jazzyspoon@users.noreply.github.com>
2021-10-31 11:37:36 -0600

    Update mp3-player.component.ts

--- src/app/mp3-player/mp3-player.component.ts
+++ src/app/mp3-player/mp3-player.component.ts
@@ -33,8 +33,8 @@ export class Mp3PlayerComponent implements OnInit {
       duration: 3,
     },
     {
-      title: 'Elena Beats',
-      link: 'https://jazzyspoon.com/tools/BEATS%20FOR%20ELENA%201.mp3',
+      title: 'Absence',
+      link: 'https://jazzyspoon.com/tools/HalloJams/Jazzyspoon%20-%20Hard%20Drive%20Recovery%20-%2006%20Absence%20(20%20mix).mp3',
       artist: 'Jazzyspoon',
       duration: 3,
     },
@@ -51,6 +51,12 @@ export class Mp3PlayerComponent implements OnInit {
       duration: 3,
     },
     {
+      title: 'Our Whispered Rants',
+      link: 'https://jazzyspoon.com/tools/HalloJams/Jazzyspoon%20-%20Noise%20and%20Noir%20-%2002%20Our%20Whispered%20Rants.mp3',
+      artist: 'Jazzyspoon',
+      duration: 3,
+    },
+    {
       title: 'Gone',
       link: 'https://jazzyspoon.com/tools/HalloJams/Jazzyspoon%20&%20IG88%20-%20Jazzyspoon%20&%20IG88%20-%2001%20Gone.mp3',
       artist: 'Jazzyspoon and IG88',
@@ -60,18 +66,6 @@ export class Mp3PlayerComponent implements OnInit {
       title: 'Sleeper',
       link: 'https://jazzyspoon.com/tools/HalloJams/08%20-%20Sleeper.mp3',
       artist: 'Yeah Ghost',
-      duration: 3,
-    },
-    {
-      title: 'Absence',
-      link: 'https://jazzyspoon.com/tools/HalloJams/Jazzyspoon%20-%20Hard%20Drive%20Recovery%20-%2006%20Absence%20(20%20mix).mp3',
-      artist: 'Jazzyspoon',
-      duration: 3,
-    },
-    {
-      title: 'Our Whispered Rants',
-      link: 'https://jazzyspoon.com/tools/HalloJams/Jazzyspoon%20-%20Noise%20and%20Noir%20-%2002%20Our%20Whispered%20Rants.mp3',
-      artist: 'Jazzyspoon',
       duration: 3,
     },
   ];
